dd1fc0ed76f06d457d212b34d3104d648b99ea46
Mikel Barrio <mikelbcss@gmail.com>
2024-03-02 02:58:21 +0100

    Se configura la página

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en", "fr"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  integrations: [tailwind()]
+});
